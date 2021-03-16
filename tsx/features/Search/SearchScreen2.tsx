81807129ed76533de996a1d67280d9c790ae37ae
Ultoman <jabonasera@gmail.com>
2021-03-16 02:05:38 -0500

    App done

--- tsx/features/Search/SearchScreen2.tsx
+++ tsx/features/Search/SearchScreen2.tsx
@@ -1,11 +1,14 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { Text, View } from 'react-native'
 import {NavigationScreenProp} from 'react-navigation'
 import Button from '../../components/Button'
 import AutoCompleteInputField from '../../components/AutoCompleteInputField'
-import { setLocationTwo } from '../../testActions'
+import { setLocationTwo } from '../../../testActions'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCenter } from 'geolib';
+import { placeSearch } from '../../api/PlaceSearch';
+import { Input } from 'react-native-elements';
+
 
 
 interface Props {
@@ -13,6 +16,7 @@ interface Props {
 }
 
 export default function SearchScreen2({ navigation }: Props): ReactElement {
+    const [search, setSearch] = useState('');
     const dispatch = useDispatch()
     const locationOne = useSelector(state => state.testReducer.locationOne)
     const locationTwo = useSelector(state => state.testReducer.locationTwo)
@@ -22,6 +26,11 @@ export default function SearchScreen2({ navigation }: Props): ReactElement {
         dispatch(setLocationTwo(location))
     }
 
+    const handleSearch = async () => {
+        console.log(search);
+        const result = await placeSearch(search, 'en', location);
+        console.log(JSON.stringify(result.data));
+    }
 
    
 
@@ -41,6 +50,8 @@ export default function SearchScreen2({ navigation }: Props): ReactElement {
                 <Text>Location Two: {locationTwo?.formatted_address ?? 'Not yet selected'}</Text>
                 <Text>{location?.latitude} {location?.longitude}</Text>
                 <AutoCompleteInputField setLocation={setLocation} />
+                <Input onChangeText={value => setSearch(value)}/>
+                <Button type='secondary' title='Search' disabled={!location} onPress={handleSearch} />
             <Button type='primary' title="Continue" disabled={!locationOne} buttonStyle={{marginBottom: 20}} onPress={() => {}}/>
             </View>
             
