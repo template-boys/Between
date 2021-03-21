6fd429856a83c9c5969fe996899a56a52a6299c9
James Bonasera <jabonasera@gmail.com>
2021-03-21 01:28:41 -0500

    Added SearchResultsScreen (#16)
    * Added SearchResultsScreen
    
    * Added back button

--- tsx/navigation/SearchNavigator.tsx
+++ tsx/navigation/SearchNavigator.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Icon from "react-native-vector-icons/Ionicons";
 import SearchScreen from "../features/Search/SearchScreen";
-import SearchScreen2 from "../features/Search/SearchScreen2";
+import SearchResultsScreen from "../features/Search/SearchResultsScreen";
 import theme from "../themes/theme";
 
 interface Props {}
@@ -11,26 +11,28 @@ const SearchStack = createStackNavigator();
 
 export default function SearchNavigator({}: Props): ReactElement {
   return (
-    <SearchStack.Navigator>
-      <SearchStack.Screen
-        name="In Between"
-        component={SearchScreen}
-        options={{
+    <SearchStack.Navigator
+      screenOptions={(props) => {
+        return {
           headerLeft: ({ canGoBack }) =>
             canGoBack && (
-              <Icon name="arrow-back-outline" size={30} color={theme.purple} />
+              <Icon
+                name="arrow-back-outline"
+                size={30}
+                color={theme.purple}
+                onPress={() => {
+                  props.navigation.goBack();
+                }}
+              />
             ),
-        }}
-      />
+          headerLeftContainerStyle: { paddingLeft: 10 },
+        };
+      }}
+    >
+      <SearchStack.Screen name="In Between" component={SearchScreen} />
       <SearchStack.Screen
         name="Search Screen 2"
-        component={SearchScreen2}
-        options={{
-          headerLeft: ({ canGoBack }) =>
-            canGoBack && (
-              <Icon name="arrow-back-outline" size={30} color={theme.purple} />
-            ),
-        }}
+        component={SearchResultsScreen}
       />
     </SearchStack.Navigator>
   );
