a5d348415baf1a6b8937e9766dee4b0720ba58de
justin-dupre <42753529+justin-dupre@users.noreply.github.com>
2021-09-15 01:01:32 -0500

    Newbranch (#42)
    * increment and certs
    
    * added push noti code

--- ios/Between/AppDelegate.m
+++ ios/Between/AppDelegate.m
@@ -52,6 +52,27 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  if ([UNUserNotificationCenter class] != nil) {
+  // iOS 10 or later
+  // For iOS 10 display notification (sent via APNS)
+  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+  UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
+      UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+  [[UNUserNotificationCenter currentNotificationCenter]
+      requestAuthorizationWithOptions:authOptions
+      completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        // ...
+      }];
+  } else {
+    // iOS 10 notifications aren't available; fall back to iOS 8-9 notifications.
+    UIUserNotificationType allNotificationTypes =
+    (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+    UIUserNotificationSettings *settings =
+    [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+    [application registerUserNotificationSettings:settings];
+  }
+
+  [application registerForRemoteNotifications];
   return YES;
 }
 
